80fdfe9daf1b717daa5cf4c9d20a60cca04c9910
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-07-25 00:00:34 -0700

    feat: add custom black and orange colors to Tailwind CSS theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-black': '#333333',
+        'custom-orange': '#FF5722',
+      },
+    },
   },
   plugins: [],
 }
