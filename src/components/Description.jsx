dc1f65b389e7387ea72bec3e589f9a7bfec7297a
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-07-25 03:07:39 -0700

    feat(Description.jsx): create Description component

---
+++ src/components/Description.jsx
@@ -0,0 +1,15 @@
+import H2 from "../assets/descriptionImage/H2.png";
+import items from "../assets/descriptionImage/items.png"
+const Description = () => {
+  return (
+    <>
+      <div className="block md:hidden flex flex-col items-center space-y-10 mt-12">
+        <img src={H2} alt="h2" className="md:w-auto md:h-auto " />
+        <p className="text-custom-black font-bold">ما میتوانیم با تیم حرفه ای خود به شما برای تحقق <br />اهدافتان کمک کنیم</p>
+        <img src={items} alt=""/>
+      </div>
+    </>
+  );
+};
+
+export default Description;
