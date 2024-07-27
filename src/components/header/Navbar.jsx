8e7eed98dc86fe9b75a39423418ca75cba4fc2b5
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-07-27 13:59:52 -0700

    refactor(Navbar.jsx): improve responsiveness

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,48 +1,55 @@
 import { useState } from "react";
 import Rectangle from "../../assets/headerImage/Rectangle.png";
 import { GiHamburgerMenu } from "react-icons/gi";
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <>
-      <nav className="fixed top-0 left-0 right-0 bg-white z-50">
-        <div className="container mx-auto flex justify-between items-center">
+    <div className="flex justify-center w-full">
+      <nav className="bg-white z-50 w-[744px] h-[50px] flex items-center justify-center">
+        <div className="flex justify-between items-center w-full h-full">
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="md:hidden focus:outline-none mr-6 mt-6"
+            className="md:hidden focus:outline-none mr-6"
           >
             <GiHamburgerMenu className="w-6 h-6" />
           </button>
-          <div className="text-white font-semibold space-x-6 mr-10 lg:mr-36 hidden md:flex justify-end gap-8">
-            <a href="#" className="text-black hover:text-gray-300 ml-6">
-              تیم
-            </a>
-            <a href="#" className="text-black hover:text-gray-300">
-              خدمات
-            </a>
-            <a href="#" className="text-black hover:text-gray-300">
-              نظرات
-            </a>
+
+          <div className="flex items-center justify-center">
+            <div className="text-white font-semibold space-x-6 hidden md:flex gap-8">
+              <a href="#" className="text-black hover:text-gray-300 ml-6 w-[21px] h-[32px] text-sm">
+                تیم
+              </a>
+              <a href="#" className="text-black hover:text-gray-300 w-[46px] h-[32px] text-sm">
+                خدمات
+              </a>
+              <a href="#" className="text-black hover:text-gray-300 w-[39px] h-[32px] text-sm">
+                نظرات
+              </a>
+            </div>
           </div>
 
-          <div className="hidden justify-center items-center md:flex">
-            <img src={Rectangle} alt="Logo" className=" object-contain" />
-          </div>
+          <div className="flex">  <div className="hidden md:block justify-center items-center w-[198px]">
+            <img src={Rectangle} alt="Logo" className="object-contain h-[50px] " />
+          </div></div>
 
-          <div className="space-x-6 ml-10 lg:ml-36  font-bold hidden md:flex justify-end gap-8">
-            <a href="#" className="text-black hover:text-gray-300 ml-6">
+        
+
+          <div className="hidden md:flex space-x-6 ml-10 font-bold justify-end gap-8">
+            <a href="#" className="text-black hover:text-gray-300 ml-6 mr-[2rem] w-[45px] h-[32px] text-sm">
               تخفیف
             </a>
-            <a href="#" className="text-black hover:text-gray-300">
+            <a href="#" className="text-black hover:text-gray-300 w-[69px] h-[32px] text-sm">
               نمونه کارها
             </a>
-            <a href="#" className="text-black hover:text-gray-300">
+            <a href="#" className="text-black hover:text-gray-300 w-[35px] h-[32px] text-sm">
               ارتباط
             </a>
           </div>
         </div>
       </nav>
-    </>
+    </div>
   );
 };
 
