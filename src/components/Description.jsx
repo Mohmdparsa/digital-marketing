f96ade767fc0716e0429535797281c54a37b1e60
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-07-28 05:13:10 -0700

    style(Description.jsx): add margin bottom

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -3,7 +3,7 @@ import items from "../assets/descriptionImage/items.png"
 const Description = () => {
   return (
     <>
-      <div className="block md:hidden flex flex-col items-center space-y-10 mt-12">
+      <div className="block md:hidden flex flex-col items-center space-y-10 mt-12 mb-[10rem]">
         <img src={H2} alt="h2" className="md:w-auto md:h-auto " />
         <p className="text-custom-black font-bold">ما میتوانیم با تیم حرفه ای خود به شما برای تحقق <br />اهدافتان کمک کنیم</p>
         <img src={items} alt=""/>
