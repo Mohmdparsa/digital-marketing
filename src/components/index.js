1ee9ded589054403f94d02380e382de83a1bd6b1
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-07-28 10:14:02 -0700

    refactor(index.js): organize component exports

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+
+export {default as Navbar} from "./Navbar"
+export {default as FirstPage} from "./FirstPage"
+export {default as FirstPageBoxes} from "./FirstPageBoxes"
+export {default as TeamImage} from "./TeamImage.jsx" 
+export {default as Description} from "./Description"
+export {default as Services} from "./Services.jsx"
+export {default as ViewCarousel} from "./ViewCarousel.jsx"
+export {default as OfferSection} from "./OfferSection"
+export {default as Portfolio} from "./Portfolio"
+export {default as Guidance} from './Guidance'
+export {default as Footer} from "./Footer"
+
+
+
+
+
+
+
