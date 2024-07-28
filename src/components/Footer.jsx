20206da53be11fef24009af0e70d3b8a60dba1ae
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-07-28 07:44:39 -0700

    refactor(Footer.jsx): make item be in center for better responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,30 +7,18 @@ import whatsapp from "../assets/footerImage/whatsapp.png";
 const Footer = () => {
   return (
     <>
-      <hr className="border-t border-custom-blue mt-[8rem]" />
-      <div className="w-[360px] h-[140px] md:w-[1366px] md:h-[100px] flex flex-col md:flex-row items-center md:items-start mt-[2rem]">
-        <div className="mt-[2rem] mr-[5rem]">
-          <img
-            src={logo}
-            alt="logo"
-            className="w-[187px] h-[32px] hidden md:block"
-          />
+      <hr className="border-t border-custom-blue mt-32" />
+      <div className="flex flex-col items-center mt-8 md:flex-row md:justify-between md:items-center w-full max-w-screen-lg mx-auto px-4 mb-[2rem]">
+        <div className="mt-8 md:mt-0">
+          <img src={logo} alt="logo" className="w-48 hidden md:block" />
         </div>
-        <div className="mt-[2.5rem] mr-[5rem] md:mr-[15rem]">
-          <img src={copyright} alt="copyright" className="w-[298px] h-[24px]" />
+        <div className="mt-10 md:mt-0">
+          <img src={copyright} alt="copyright" className="w-72" />
         </div>
-        <div className="flex w-[168px] h-[40px] mt-[2rem] mr-[8rem] md:mr-[15rem]">
-          <img src={whatsapp} alt="whatsapp" className="w-[40px] h-[40px]" />
-          <img
-            src={telegramLogo}
-            alt="telegranLogo"
-            className="w-[40px] h-[40px]"
-          />
-          <img
-            src={instagramlogo}
-            alt="instagram"
-            className="w-[40px] h-[40px]"
-          />
+        <div className="flex space-x-4 mt-8 md:mt-0">
+          <img src={whatsapp} alt="whatsapp" className="w-10 h-10 ml-[1rem]" />
+          <img src={telegramLogo} alt="telegramLogo" className="w-10 h-10" />
+          <img src={instagramlogo} alt="instagram" className="w-10 h-10" />
         </div>
       </div>
     </>
