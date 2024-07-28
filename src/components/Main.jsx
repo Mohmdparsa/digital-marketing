631e511e1c7336159b1961f45e4c996e51c225a0
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-07-28 10:15:52 -0700

    rafactor(Main.js): import components from index.js

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,31 @@
-import Navbar from "./Navbar";
-import FirstPage from "./FirstPage";
-import FirstPageBoxes from "./FirstPageBoxes";
-import TeamImage from "./TeamImage"
-import Description from "./Description"
-import Services from "./Services"
-import ViewCarousel from "./ViewCarousel"
-import OfferSection from "./OfferSection"
-import Portfolio from "./Portfolio";
-import Guidance from "./Guidance";
-import Footer from "./Footer";
+import {
+  Navbar,
+  FirstPage,
+  FirstPageBoxes,
+  TeamImage,
+  Description,
+  Services,
+  ViewCarousel,
+  OfferSection,
+  Portfolio,
+  Guidance,
+  Footer,
+} from "./index.js";
 const Main = () => {
   return (
     <>
       <div className="flex flex-col">
         <Navbar />
         <FirstPage />
-        <FirstPageBoxes/>
-        <TeamImage/>
-        <Description/>
-        <Services/>
-        <ViewCarousel/>
-        <OfferSection/>
-        <Portfolio/>
-        <Guidance/>
-        <Footer/>
-       
+        <FirstPageBoxes />
+        <TeamImage />
+        <Description />
+        <Services />
+        <ViewCarousel />
+        <OfferSection />
+        <Portfolio />
+        <Guidance />
+        <Footer />
       </div>
     </>
   );
