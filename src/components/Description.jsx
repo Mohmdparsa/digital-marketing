203fb9d0e7ed76149b48e19c41bf696060e358e4
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-07-31 01:58:37 -0700

    refactor(Descritpiton.jsx): remove images and add texts

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,12 +1,31 @@
-import H2 from "../assets/descriptionImage/H2.png";
-import items from "../assets/descriptionImage/items.png"
 const Description = () => {
   return (
     <>
       <div className="block md:hidden flex flex-col items-center space-y-10 mt-12 mb-[10rem]">
-        <img src={H2} alt="h2" className="md:w-auto md:h-auto " />
-        <p className="text-custom-black font-bold">ما میتوانیم با تیم حرفه ای خود به شما برای تحقق <br />اهدافتان کمک کنیم</p>
-        <img src={items} alt=""/>
+        <div className="flex flex-col itemn-center justify-center">
+          <h1 className="text-3xl mr-[3rem] font-bold">
+            <span className="text-custom-blue">تیم متخصص </span>با
+          </h1>
+          <h1 className="text-3xl font-bold">توانایی های منحصر بفرد</h1>
+        </div>
+        <p className="text-custom-black font-bold">
+          ما میتوانیم با تیم حرفه ای خود به شما برای تحقق <br />
+          اهدافتان کمک کنیم
+        </p>
+
+        <div className="mr-[1rem] mt-[2rem]">
+          <ul className="list-disc marker:text-red-500">
+            <li className="mt-[0.5rem] text-md">
+              خلاقیت : می توانیم راه حل های جدید و خلاقانه ارایه بدیم
+            </li>
+            <li className="mt-[0.5rem] text-md">
+              دانش : با دانش روز دنیا توانایی حل هرگونه مسله رو داریم
+            </li>
+            <li className="mt-[0.5rem] text-md">
+              همکاری : کار تیمی برای به نتیجه رسیدن در سریعترین زمان
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
